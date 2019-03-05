98ea6fffc2c701c9f490231dc3e1fca4713c6f6b
Francisco Javier Vadillo <32244264+FranVadillo@users.noreply.github.com>
2019-03-05 15:58:54 +0100

    Nueva versión del portfolio
    Cambios hechos desde la charla

--- js/popup.js
+++ js/popup.js
@@ -1,8 +1,10 @@
 window.onload = function(){
     document.onclick = function(e){
         console.log(e.target.className);
-        if(e.target.className !== "popup" && e.target.className !== "random" &&
-            e.target.className !== "carousel-control-next-icon" && e.target.className !== "carousel-control-next"){
+        if(e.target.className !== "popup" && e.target.className !== "imageInRandom"
+            && e.target.className !== "textInRandom" && e.target.className !== "carousel-control-next-icon" 
+            && e.target.className !== "carousel-control-next" && e.target.className !== "carousel-control-prev-icon" 
+            && e.target.className !== "carousel-control-prev"){
             d = document.getElementsByClassName("popup");
             for(let i = 0; i < d.length; i++){
                 d[i].style.display = 'none';
